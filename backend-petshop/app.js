ed580efe16703de59d5c061cc77563cb531676c7
herrsaribas <ahmetmsaribas@outlook.com>
2023-01-11 10:15:15 +0100

    new feature to cat added

--- backend-petshop/app.js
+++ backend-petshop/app.js
@@ -1,36 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import multer from 'multer';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import multer from "multer";
+import cors from "cors";
 
 //import path: __dirname
-import path from 'path';
+import path from "path";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 //to access env parameters
 dotenv.config();
 
 //DB connection
-import './models/dbConnection.js';
+import "./models/dbConnection.js";
 
 //import : user defined function
-import userRoute from './routes/userRoute.js';
+import userRoute from "./routes/userRoute.js";
 
 //create and initialize express server
 const app = express();
 
 //cors config
-app.use(cors({ origin: 'http://localhost:3000', exposedHeaders: ['token'] }));
+app.use(cors({ origin: "http://localhost:3001", exposedHeaders: ["token"] }));
 
 // configure multer package
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    let fullPath = './upload';
+    let fullPath = "./upload";
     cb(null, fullPath);
   },
   filename: function (req, file, cb) {
-    let fileName = Date.now() + '_' + file.originalname;
+    let fileName = Date.now() + "_" + file.originalname;
     cb(null, fileName);
   },
 });
@@ -38,22 +38,22 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // server static files/pages
-app.use(express.static('upload'));
+app.use(express.static("upload"));
 
 //external middleware
 //req-log middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 //parse json middleware
 app.use(express.json());
 
 //routes - custom middleware
 
 //GET ,POST,PATCH,DELETE - req '/user'endpoint and its controller
-app.use('/users', upload.single('profileImage'), userRoute);
+app.use("/users", upload.single("profileImage"), userRoute);
 
 //page not found
 app.use((req, res, next) => {
-  res.sendFile('views/pageNotFound.html', { root: __dirname }); //sendFile needs absolute path of the File+
+  res.sendFile("views/pageNotFound.html", { root: __dirname }); //sendFile needs absolute path of the File+
 });
 
 //universal Error handler - custom middleware with error as a parameter
