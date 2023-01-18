da21bbccc2a4a4c76e3a7767a28d22d7597743d3
Velam <velam.m@gmail.com>
2023-01-18 09:22:09 +0100

    initial commit with some frontend dog page

--- frontend-petshop/src/App.js
+++ frontend-petshop/src/App.js
@@ -1,53 +1,38 @@
-import { Routes, Route , Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-
 import Main from "./components/main/Main.js";
 import Login from "./components/user/Login.js";
-import Signup from "./components/user/Signup.js"
-import Profile from "./components/user/Profile.js"
+import Signup from "./components/user/Signup.js";
+import Profile from "./components/user/Profile.js";
 import Account from "./components/user/Account.js";
 import Notification from "./components/user/Notification.js";
-
-
+import Dog from "./components/petType/Dog";
 
 function App() {
   return (
-    <div className="    h-screen md:w-[100vw] flex flex-col" >
-      
-      <div className=" ">
+    <div className="    h-screen md:w-[100vw] flex flex-col">
       <Header />
-      </div>
-      
-      
-        
       <div className="w-3/4 mx-auto mt-[15rem] ">
-      <Routes>
-        <Route path="/">
-          <Route
-            index={true}
-            path="/"
-            className="min-h-screen"
-            element={<Main />}
-          />
-        </Route>
-        <Route index={false} path="login" element={<Login />} />
-        <Route index={false} path="signup" element={<Signup />} />
+        <Routes>
+          <Route path="/">
+            <Route index={true} path="/" className="min-h-screen" element={<Main />} />
+          </Route>
+          <Route index={false} path="login" element={<Login />} />
+          <Route index={false} path="signup" element={<Signup />} />
+          <Route index={false} path="profile" element={<Profile />} />
+          <Route index={false} path="account" element={<Account />} />
+          <Route index={false} path="notification" element={<Notification />} />
 
-        <Route index={false} path="profile" element={<Profile />} /> 
-        <Route index={false} path="account" element={<Account />} />
-        <Route index={false} path="notification" element={<Notification />} />
-    
-        <Route path="*" element={<Navigate to="/" replace />}
-    />
-        
-      </Routes>
+           <Route index={false} path="dog" element={<Dog />} /> 
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
-        <Footer /> 
-       
+      <Footer />
     </div>
   );
 }
--- frontend-petshop/src/components/header/Header.js
+++ frontend-petshop/src/components/header/Header.js
@@ -18,32 +18,39 @@ export default function Header() {
 
   return (
     <div className="flex flex-col md:justify-between items-center  md:shadow  mb-3  z-50 w-[100vw] fixed top-0 bg-white ">
-       <div onClick={homePageHandler} className=" font-bold  flex md:hidden justify-center items-center">
-          <img src={Logo} alt="header-icon" className=" w-20 mr-2" />
-          <p className="text-2xl">Pet Store </p>
-        </div>
+      <div
+        onClick={homePageHandler}
+        className=" font-bold  flex md:hidden justify-center items-center"
+      >
+        <img src={Logo} alt="header-icon" className=" w-20 mr-2" />
+        <p className="text-2xl">Pet Store </p>
+      </div>
       <div className="flex justify-between items-center md:px-[5rem] md:w-[100%] relative ">
-        <div className=" hidden lg:flex z-10"><HeaderSearch /></div>
-        
-        <div onClick={homePageHandler} className="md:text-4xl font-bold  md:p-3  hidden md:flex  justify-center items-center">
+        <div className=" hidden lg:flex z-10">
+          <HeaderSearch />
+        </div>
+
+        <div
+          onClick={homePageHandler}
+          className="md:text-4xl font-bold  md:p-3  hidden md:flex  justify-center items-center"
+        >
           <img src={Logo} alt="header-icon" className=" w-20 mr-2" />
           <p className="">Pet Store </p>
         </div>
         <div className="block md:hidden  ">
-        <Navbar />
+          <Navbar />
         </div>
         {user && <Logout />}
-        
-        
-       
+
         <HeaderMenu />
-       
       </div>
-      
-      <div className=" block lg:hidden "><HeaderSearch /></div>
+
+      <div className=" block lg:hidden ">
+        <HeaderSearch />
+      </div>
       <div className="hidden md:block">
         <Navbar />
-        </div>
+      </div>
     </div>
   );
 }
