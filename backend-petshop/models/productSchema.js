71281508057724eb7f73add568cf96964b1d8fdc
Velam <velam.m@gmail.com>
2023-01-19 10:15:12 +0100

    product backend created and product page for Dog has displayed

--- backend-petshop/models/productSchema.js
+++ backend-petshop/models/productSchema.js
@@ -9,6 +9,7 @@ const productSchema = new Schema({
     brand: { type: String, required: true },
     lifeStyle: { type: String, enum: ['puppy', 'adult', 'senior'], default: 'adult' },
     petSize: { type: String, enum: ['small', 'medium', 'large'], default: 'small' },
+    petMenu:{type:String,enum:['dog-food','dog-pharmacy','dog-toys','dog-treats-bones','dog-homes','care-maintenance','supplements']}
     /* productImage: {
         type: String, default: function () {
             return 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.collinsdictionary.com%2Fimages%2Ffull%2Fpetfood_150577460.jpg&imgrefurl=https%3A%2F%2Fwww.collinsdictionary.com%2Fde%2Fworterbuch%2Fenglisch%2Fpet-food&tbnid=DVqgAT2IGzGK4M&vet=12ahUKEwjd88O6oNH8AhU5oScCHYIACccQMygDegUIARDqAw..i&docid=xdve07u5DYysPM&w=1000&h=724&q=pet%20food&ved=2ahUKEwjd88O6oNH8AhU5oScCHYIACccQMygDegUIARDqAw'
