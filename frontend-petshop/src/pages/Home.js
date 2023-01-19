df6a4c20191325792f75d4e6440e85d6b33f64a9
Ahmet Saribas <ahmetsaribas@Ahmets-MacBook-Pro.local>
2023-01-19 09:41:07 +0100

    initial commit

--- frontend-petshop/src/pages/Home.js
+++ frontend-petshop/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Categories from "../components/main/Categories";
 import Products from "../components/main/Products";
+import Slider from "../components/main/Slider";
 
 export default function Home() {
   return (
     <div>
+      <Slider />
       <Categories />
       <Products />
+      
     </div>
   );
 }
