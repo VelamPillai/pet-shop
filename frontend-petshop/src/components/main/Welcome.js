64dcdaa5d394abbadbf25dd15254615c6b6ac08d
Velam <velam.m@gmail.com>
2023-01-23 13:25:52 +0100

    changes made in the text part of welcome component

--- frontend-petshop/src/components/main/Welcome.js
+++ frontend-petshop/src/components/main/Welcome.js
@@ -6,7 +6,7 @@ export default function Welcome() {
     <div className='grid md:grid-cols-2 mt-16 sm:grid-cols-1'>
         <div className='w-full'>
           <h2 className='text-[28px] font-bold leading-9 '>Welcome to our shop</h2>
-          <p className='text-[16px] leading-24 my-4'>At FirstVet you will only find the highest quality products for your pet. We focus on products that enhance your pet's health and wellbeing like veterinary diet dog food, veterinary diet cat food and supplements.</p>
+          <p className='text-[16px] leading-24 my-4'>At Pet Store you will only find the highest quality products for your pet. We focus on products that enhance your pet's health and wellbeing like veterinary diet dog food, veterinary diet cat food and supplements.</p>
           <p className='text-[16px] leading-24 my-4'>We also offer an assortment of more traditional products like toys, care products and cat litter.</p>
           <button className='border-2  rounded-md bg-orange-400 px-6 py-2 my-4  flex items-center justify-center gap-4  text-white'  >Shop now <HiArrowRight /></button>
         </div>
