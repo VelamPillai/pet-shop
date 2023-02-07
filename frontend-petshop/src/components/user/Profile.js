5acc6b1487188d45619a306811415228d946cc4b
Velam <velam.m@gmail.com>
2023-02-07 21:37:32 +0100

    removed notification button in profile

--- frontend-petshop/src/components/user/Profile.js
+++ frontend-petshop/src/components/user/Profile.js
@@ -58,12 +58,12 @@ export default function Profile() {
             >
               Account
             </button>
-            <button
+            {/* <button
               onClick={profileHandler}
               className="bg-gradient-to-r from-orange-500 to-yellow-600 text-xs hover:bg-gradient-to-l justify-center items-center w-[150px] md:w-[200px] m-3 md:mx-[1rem] md:my-[1rem] md:p-1 rounded shadow-black shadow-md focus:bg-green-600  h-[30px] lg:box-content"
             >
               Notification
-            </button>
+            </button> */}
             <button
               onClick={() => deleteHandler(user._id)}
               className="bg-gradient-to-r from-orange-500 to-yellow-600 text-xs hover:bg-gradient-to-l justify-center items-center w-[150px] md:w-[200px]  m-3 md:mx-auto md:my-[1rem] md:p-1 rounded shadow-black shadow-md focus:bg-green-600  h-[30px] lg:box-content"
